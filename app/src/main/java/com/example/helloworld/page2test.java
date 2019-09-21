be46ba3d4bbfeb0b6c574aec297c99019f034802
JosephVitko21 <josephvitko21@gmail.com>
2019-09-21 12:58:59 -0400

    added back button and improved text

--- app/src/main/java/com/example/helloworld/page2test.java
+++ app/src/main/java/com/example/helloworld/page2test.java
@@ -2,7 +2,10 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class page2test extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class page2test extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2test);
+        final Button backButton = (Button) findViewById(R.id.backButton);
+
+        View.OnClickListener ocl;
+        ocl = new View.OnClickListener(){
+            public void onClick(View v){
+                openActivity1();
+            }
+        };
+
+        backButton.setOnClickListener(ocl);
+
+    }
+
+    public void openActivity1(){
+
+        Intent intent = new Intent(page2test.this, MainActivity.class);
+        this.startActivity(intent);
+
     }
 }
