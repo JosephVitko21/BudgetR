51d2cc14e5804945b0b15f9192a7d739b8ff3fc3
JosephVitko21 <josephvitko21@gmail.com>
2019-09-21 19:25:58 -0400

    added view pager with mole rat pictures

---
+++ app/src/main/java/com/example/helloworld/CustomSwipeAdapter.java
@@ -0,0 +1,53 @@
+package com.example.helloworld;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class CustomSwipeAdapter extends PagerAdapter {
+    private int[] imageResources =  {R.drawable.naked_cat, R.drawable.naked_mole_rat1, R.drawable.naked_mole_rat2, R.drawable.naked_mole_rat3};
+    private Context ctx;
+    private LayoutInflater layoutInflater;
+
+    public CustomSwipeAdapter(Context ctx){
+        this.ctx=ctx;
+    }
+    @Override
+    public int getCount() {
+        return imageResources.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object o) {
+        return (view==(LinearLayout)o);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View itemView = layoutInflater.inflate(R.layout.swipe_layout,container,false);
+        ImageView imageView = (ImageView)itemView.findViewById(R.id.imageView);
+        TextView textView = (TextView)itemView.findViewById(R.id.image_count);
+        imageView.setImageResource(imageResources[position]);
+        textView.setText("Image: + " + position);
+        container.addView(itemView);
+        return itemView;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((LinearLayout)object);
+
+
+        super.destroyItem(container, position, object);
+    }
+}
--- app/src/main/java/com/example/helloworld/tutorialActivity.java
+++ app/src/main/java/com/example/helloworld/tutorialActivity.java
@@ -2,16 +2,24 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
 public class tutorialActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
+    CustomSwipeAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        adapter = new CustomSwipeAdapter(this);
+        viewPager.setAdapter(adapter);
+
+
     }
 }
