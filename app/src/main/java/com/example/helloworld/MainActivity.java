0f5424c54b7a60703ad331b31c15bc47cc720319
JosephVitko21 <josephvitko21@gmail.com>
2019-09-21 12:24:00 -0400

    added new page

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
 import android.os.Bundle;
+import android.content.Intent;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final Button button = (Button) findViewById(R.id.button);
         final TextView helloWorldLabel = (TextView) findViewById(R.id.helloWorldLabel );
+        final Button activity2button = (Button) findViewById(R.id.activity2button);
 
         View.OnClickListener ocl;
         ocl = new View.OnClickListener()
@@ -32,6 +34,27 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
+
+        View.OnClickListener newOcl;
+        newOcl = new View.OnClickListener()
+        {
+
+            public void onClick(View v)
+            {
+                openActivity2();
+
+
+            }
+        };
+
         button.setOnClickListener(ocl);
+        activity2button.setOnClickListener(newOcl);
+    }
+
+    public void openActivity2() {
+
+        Intent intent = new Intent(this, page2test.class);
+        this.startActivity(intent);
+
     }
 }
