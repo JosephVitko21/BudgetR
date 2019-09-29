84765ddfeb055c8c5a53dd0eb650e4772a7e477a
JosephVitko21 <josephvitko21@gmail.com>
2019-09-28 20:05:04 -0400

    added view pager to home page

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -9,6 +9,9 @@ import android.os.Bundle;
 import android.content.Intent;
 import me.relex.circleindicator.CircleIndicator;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
         viewPager = (ViewPager) findViewById(R.id.viewPager);
         adapter = new CustomSwipeAdapter(this);
         viewPager.setAdapter(adapter);
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new MyTimerTask(), 2000, 4000);
 
         indicator.setViewPager(viewPager);
 
@@ -65,5 +71,30 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, tutorialActivity.class);
         this.startActivity(intent);
 
+    }
+
+    public class MyTimerTask extends TimerTask{
+
+        @Override
+        public void run() {
+
+            MainActivity.this.runOnUiThread(new Runnable(){
+
+                @Override
+                public void run() {
+
+                    if(viewPager.getCurrentItem() == 0){
+                        viewPager.setCurrentItem(1);
+                    } else if(viewPager.getCurrentItem() == 1){
+                        viewPager.setCurrentItem(2);
+                    } else if(viewPager.getCurrentItem() == 2){
+                        viewPager.setCurrentItem(0);
+                    }
+                }
+
+
+            });
+
+        }
     }
 }
