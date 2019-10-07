ce522d5edaba50f42323b0cc81d246f5a92e0f55
JosephVitko21 <josephvitko21@gmail.com>
2019-10-06 23:22:02 -0400

    Added tabbed layout

---
+++ app/src/main/java/com/example/helloworld/ViewPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    public ViewPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        DemoFragment demoFragment = new DemoFragment();
+        position = position+1;
+        Bundle bundle = new Bundle();
+        bundle.putString("message","Fragment: " +position);
+        demoFragment.setArguments(bundle);
+        return demoFragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+
+        position = position+1;
+
+        return "Fragment "+position;
+    }
+}
--- app/src/main/java/com/example/helloworld/page2test.java
+++ app/src/main/java/com/example/helloworld/page2test.java
@@ -1,35 +1,38 @@
 package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class page2test extends AppCompatActivity {
 
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private ViewPagerAdapter adapter;
+    private TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2test);
-        final Button backButton = (Button) findViewById(R.id.backButton);
 
-        View.OnClickListener ocl;
-        ocl = new View.OnClickListener(){
-            public void onClick(View v){
-                openActivity1();
-            }
-        };
+        toolbar=findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
 
-        backButton.setOnClickListener(ocl);
+        viewPager = findViewById(R.id.tabbedPager);
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
 
-    }
+        tabLayout = findViewById(R.id.tabs);
 
-    public void openActivity1(){
-
-        Intent intent = new Intent(page2test.this, MainActivity.class);
-        this.startActivity(intent);
+        tabLayout.setupWithViewPager(viewPager);
 
     }
+
 }
