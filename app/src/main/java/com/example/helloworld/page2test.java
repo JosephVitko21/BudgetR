1217b8f9ffe1e10a31ca9a5718ffc5fba20262aa
Nihal <reddynihal34@gmail.com>
2019-10-06 21:13:17 -0400

    added the tab

--- app/src/main/java/com/example/helloworld/page2test.java
+++ app/src/main/java/com/example/helloworld/page2test.java
@@ -1,6 +1,7 @@
 package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,24 +13,19 @@ public class page2test extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_page2test);
-        final Button backButton = (Button) findViewById(R.id.backButton);
+        setContentView(R.layout.activity_main);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = findViewById(R.id.fab);
 
-        View.OnClickListener ocl;
-        ocl = new View.OnClickListener(){
-            public void onClick(View v){
-                openActivity1();
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
-        };
-
-        backButton.setOnClickListener(ocl);
-
+        });
     }
-
-    public void openActivity1(){
-
-        Intent intent = new Intent(page2test.this, MainActivity.class);
-        this.startActivity(intent);
-
-    }
-}
