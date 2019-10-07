8f45a1bd636f7de9e05ad80bcfc6f535d4cdd6a1
JosephVitko21 <josephvitko21@gmail.com>
2019-10-06 22:28:24 -0400

    undid all of today's changes

--- app/src/main/java/com/example/helloworld/page2test.java
+++ app/src/main/java/com/example/helloworld/page2test.java
@@ -1,40 +1,35 @@
 package com.example.helloworld;
 
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
-
-import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
-
-import com.example.tabs.ui.main.SectionsPagerAdapter;
+import android.widget.Button;
 
 public class page2test extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        setContentView(R.layout.activity_page2test);
+        final Button backButton = (Button) findViewById(R.id.backButton);
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-
-
+        View.OnClickListener ocl;
+        ocl = new View.OnClickListener(){
+            public void onClick(View v){
+                openActivity1();
             }
-        });
+        };
+
+        backButton.setOnClickListener(ocl);
+
     }
+
+    public void openActivity1(){
+
+        Intent intent = new Intent(page2test.this, MainActivity.class);
+        this.startActivity(intent);
+
+    }
+}
