d2008051ef43768317bc598907860920ca007859
JosephVitko21 <josephvitko21@gmail.com>
2019-10-12 11:20:09 -0400

    Reverted changes

--- app/src/main/java/com/example/helloworld/DemoFragment.java
+++ app/src/main/java/com/example/helloworld/DemoFragment.java
@@ -8,10 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 /**
@@ -19,37 +16,18 @@ import android.widget.Toast;
  */
 public class DemoFragment extends Fragment {
 
-    string name;
-    int budgetNumber;
-
-    EditText nameInput;
-    EditText budgetInput;
-
-    Button beginButton;
-
-
     private TextView textView;
 
 
     public DemoFragment() {
-
-        nameInput = (EditText) findViewById(R.idea.nameInput);
-        budgetInput = (EditText) findViewById(R.idea.budgetInput);
-
-        beginButton = (Button) findViewById(R.id.begin);
-        beginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                name = nameInput.getText().toString();
-                budgetNumber = Integer.valueOf(budgetInput.getText().toString());
-
-            }
-
+        // Required empty public constructor
+    }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_demo, container, false);
         textView = view.findViewById(R.id.txt_display);
         textView.setText(getArguments().getString("message"));
