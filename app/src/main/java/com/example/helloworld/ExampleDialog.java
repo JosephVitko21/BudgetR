2088ee9f91e1ac882b590c488ffc03c30c0b5e35
JosephVitko21 <josephvitko21@gmail.com>
2019-10-19 20:23:58 -0400

    Added custom layout to add expense pop up

--- app/src/main/java/com/example/helloworld/ExampleDialog.java
+++ app/src/main/java/com/example/helloworld/ExampleDialog.java
@@ -4,6 +4,10 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatDialogFragment;
 
@@ -11,24 +15,27 @@ public class ExampleDialog extends AppCompatDialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Add an Expense")
-                .setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
 
-                    }
-                });
-        String[] inputs = {"Scan a Receipt", "Add Expense Manually"};
-        builder.setItems(inputs, new DialogInterface.OnClickListener() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.custom_dialog_layout, null));
+        builder.setPositiveButton("Sign in", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                switch (which) {
-                    case 0: // scan
-                    case 1: // manual
-                }
+
             }
         });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+
+
 
         return builder.create();
     }
